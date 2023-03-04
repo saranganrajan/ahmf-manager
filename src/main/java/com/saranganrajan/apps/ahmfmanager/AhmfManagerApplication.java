d8fd02c3a88a55392e277d52e228ae967219e861
saranganrajan <androidsaga84@gmail.com>
2023-03-04 23:51:48 +0800

    Initial Commit
    Initial Commit, Added Feign Client for Manager service

--- src/main/java/com/saranganrajan/apps/ahmfmanager/AhmfManagerApplication.java
+++ src/main/java/com/saranganrajan/apps/ahmfmanager/AhmfManagerApplication.java
@@ -1,10 +1,17 @@
 package com.saranganrajan.apps.ahmfmanager;
 
+import com.saranganrajan.apps.ahmfmanager.external.audit.feign.AuditServiceFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.saranganrajan.apps.ahmfmanager")
 @SpringBootApplication
 public class AhmfManagerApplication {
+
+	@Autowired
+	AuditServiceFeignClient feignClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AhmfManagerApplication.class, args);
